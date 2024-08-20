b4d370575f0fc038fa5bbceeb90f7039aeb52343
Po Lin <funghi1130@gmail.com>
2024-08-20 10:58:37 +0800

    Initial Commit

---
+++ Pictionary/PictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PictionaryApp.swift
+//  Pictionary
+//
+//  Created by 李柏霖 on 2024/8/20.
+//
+
+import SwiftUI
+
+@main
+struct PictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
